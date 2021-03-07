06edfdf34ec272ce7f1abc6f488bbaf770d23ab8
Nao Yanase <nao.yanase@wustl.edu>
2021-03-06 18:47:06 -0600

    changed db to atlas

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,5 +1,5 @@
-const mongoose = require("mongoose")
-
-mongoose.connect('mongodb://localhost:27017/supremeNow', () =>
-  console.log('connected to mongo!')
-)
+const mongoose = require('mongoose');
+const connection = "mongodb+srv://nyanase:supremenow-nvma@supremenow-api-cluster.pm20k.mongodb.net/supremeNow?retryWrites=true&w=majority";
+mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+    .then(() => console.log("Database Conneted Successfully"))
+    .catch(err => console.log(err)); 
