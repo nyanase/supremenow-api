70886faf2f5982d640bda7487b1e08bc114fc477
Nao Yanase <nao.yanase@wustl.edu>
2021-03-06 18:54:51 -0600

    made db url a dotenv constant

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,5 +1,7 @@
+require('dotenv/config');
+
 const mongoose = require('mongoose');
-const connection = "mongodb+srv://nyanase:supremenow-nvma@supremenow-api-cluster.pm20k.mongodb.net/supremeNow?retryWrites=true&w=majority";
+const connection = process.env.DB_URL;
 mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
     .then(() => console.log("Database Conneted Successfully"))
     .catch(err => console.log(err)); 
