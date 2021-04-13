a7e55a8440f954a7eaf3cafdf8a37c80507d8d5d
Nao Yanase <nao.yanase@wustl.edu>
2021-04-13 12:11:59 -0500

    Updated case model to delete by docket

--- routes/case.js
+++ routes/case.js
@@ -1,48 +1,30 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Case = require('../models/case');
+const Case = require("../models/case");
 
-
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const cases = await Case.find();
     res.json(cases);
   } catch (err) {
-    res.status(500).json({ message: err })
+    res.status(500).json({ message: err });
   }
-})
+});
 
-router.get('/docket/:docket', getCase, async (req, res) => {
+router.get("/docket/:docket", getCase, async (req, res) => {
   res.json(res.theCase);
 });
 
-router.get('/year/:year', getCasesByYear, async (req, res) => {
+router.get("/year/:year", getCasesByYear, async (req, res) => {
   res.json(res.cases);
 });
 
-router.get('/active', getActiveCases, async (req, res) => {
+router.get("/active", getActiveCases, async (req, res) => {
   res.json(res.cases);
 });
 
-router.post('/', async (req, res) => {
-  const newCase = new Case({
-    name: req.body.name,
-    docket: req.body.docket,
-    petitioner: req.body.petitioner,
-    respondent: req.body.respondent,
-    appellant: req.body.appellant,
-    appellee: req.body.appellee,
-    decided_by: req.body.decided_by,
-    lower_court: req.body.lower_court,
-    citation: req.body.citation,
-    granted: req.body.granted,
-    argued: req.body.argued,
-    decided: req.body.decided,
-    description: req.body.description,
-    facts: req.body.facts,
-    question: req.body.question,
-    year: req.body.year
-  });
+router.post("/", async (req, res) => {
+  const newCase = new Case(req.body);
   try {
     const savedCase = await newCase.save();
     res.status(201).json(savedCase);
@@ -51,24 +33,24 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.patch('/:id', getCase, async (req, res) => {
+router.patch("/:id", getCase, async (req, res) => {
   if (req.body.name != null) {
-    res.theCase.name = req.body.name
+    res.theCase.name = req.body.name;
   }
   if (req.body.docket != null) {
-    res.theCase.docket = req.body.docket
+    res.theCase.docket = req.body.docket;
   }
   if (req.body.petitioner != null) {
-    res.theCase.petitioner = req.body.petitioner
+    res.theCase.petitioner = req.body.petitioner;
   }
 
   try {
-    const updatedCase = await res.theCase.save()
-    res.json(updatedCase)
+    const updatedCase = await res.theCase.save();
+    res.json(updatedCase);
   } catch (err) {
-    res.status(400).json({ message: err.message })
+    res.status(400).json({ message: err.message });
   }
-})
+});
 
 // router.patch('/:id', getCase, async (req, res) => {
 //   try {
@@ -95,61 +77,68 @@ router.patch('/:id', getCase, async (req, res) => {
 //   }
 // })
 
-router.delete('/all', async (req, res) => {
+router.delete("/all", async (req, res) => {
   try {
     const cases = await Case.deleteMany({});
     res.json(cases);
   } catch (err) {
-    res.status(500).json({ message: err })
+    res.status(500).json({ message: err });
   }
-})
+});
 
+router.delete("/docket/:docket", async (req, res) => {
+  try {
+    const delCase = await Case.deleteMany({ docket: req.params.docket });
+    res.json(delCase);
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+});
 
 async function getCase(req, res, next) {
-  let theCase
+  let theCase;
   try {
-    theCase = await Case.findById(req.params.id)
-    theCase = await Case.find({'docket': req.params.docket})
+    theCase = await Case.findById(req.params.id);
+    theCase = await Case.find({ docket: req.params.docket });
     if (theCase == null) {
-      return res.status(404).json({ message: 'Cannot find case' })
+      return res.status(404).json({ message: "Cannot find case" });
     }
   } catch (err) {
-    return res.status(500).json({ message: err })
+    return res.status(500).json({ message: err });
   }
 
-  res.theCase = theCase 
-  next()
+  res.theCase = theCase;
+  next();
 }
 
 async function getCasesByYear(req, res, next) {
-  let cases
+  let cases;
   try {
-    cases = await Case.find({'year': req.params.year})
+    cases = await Case.find({ year: req.params.year });
     if (cases == null) {
-      return res.status(404).json({ message: 'Cannot find cases' })
+      return res.status(404).json({ message: "Cannot find cases" });
     }
   } catch (err) {
-    return res.status(500).json({ message: err })
+    return res.status(500).json({ message: err });
   }
 
-  res.cases = cases 
-  next()
+  res.cases = cases;
+  next();
 }
 
 async function getActiveCases(req, res, next) {
-  let cases
+  let cases;
   try {
-    cases = await Case.find({'decided': null})
+    cases = await Case.find({ decided: null });
     if (cases == null) {
-      return res.status(404).json({ message: 'Cannot find cases' })
+      return res.status(404).json({ message: "Cannot find cases" });
     }
   } catch (err) {
-    return res.status(500).json({ message: err })
+    return res.status(500).json({ message: err });
   }
 
-  res.cases = cases 
-  next()
+  res.cases = cases;
+  next();
 }
-
 
 module.exports = router;
